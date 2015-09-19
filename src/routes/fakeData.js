b2f31fe89b4eaf5c9037c0741038b157b9757b83
Michel Boudreau <michelboudreau@gmail.com>
2015-09-19 15:06:33 +1000

    adding dynamodb class

--- src/routes/fakeData.js
+++ src/routes/fakeData.js
@@ -1,8 +1,6 @@
 /* global __dirname:true */
 var restify = require('restify'),
-		ddb = require('dynamodb').ddb({ accessKeyId: 'AKIAI5LQN2STTRV3QTGQ',
-		secretAccessKey: 'sC0UsVuzHcDgyIp5DoHdx3zE6PWq8PU5BQ099Io7',
-		endpoint: 'dynamodb.ap-southeast-2.amazonaws.com' });
+	ddb = require('../services/dynamodb')
 
 // { score: 304, date: (new Date).getTime(), sha: '3d2d6963', usr: 'spolu', lng: ['node', 'c++'] }
 
@@ -14,19 +12,20 @@ module.exports = function (server, logger) {
 		//	return next(err);
 		//}
 
-		var item = {"id": {"N": 1234},"title": {"S": "Foobar"}};
+		var item = {"id": {"N": 1234}, "title": {"S": "Foobar"}};
 
 		console.log(item);
-		var params = {TableName: 'opensalary-data',
+		var params = {
+			TableName: 'opensalary-data',
 			Item: item
 		};
 		console.log(params);
 
-		ddb.putItem(params, function(err, data) {
-    if (err)
-			console.log(err); // an error occurred
-    else
-			console.log(data); // successful response
+		ddb.putItem(params, function (err, data) {
+			if (err)
+				console.log(err); // an error occurred
+			else
+				console.log(data); // successful response
 		});
 	});
 };
---
+++ src/services/dynamodb.js
@@ -0,0 +1,7 @@
+var AWS = require('aws-sdk');
+
+module.exports = new AWS.DynamoDB({
+	accessKeyId: 'AKIAI5LQN2STTRV3QTGQ',
+	secretAccessKey: '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',
+	region: 'ap-southeast-2'
+});
